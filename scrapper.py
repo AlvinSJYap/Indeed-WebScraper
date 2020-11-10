62fadedaf5ec4cf14becbb7838bd380cce4573e6
Alvin <50644154+AlvinSJYap@users.noreply.github.com>
2020-11-10 01:57:43 -0500

    Narrow down tags needed
    Found the specific tags i want to target and the info.

--- scrapper.py
+++ scrapper.py
@@ -17,13 +17,27 @@ l=Toronto%2C+ON is the location (before the %2C+ is the city, after is the provi
 """
 
 import requests as req
+from bs4 import BeautifulSoup
 
 URL = "https://ca.indeed.com/jobs?q=Data+Analyst&l=Toronto%2C+ON"
 page = req.get(URL)
 
 
-print(page.text)
+soup = BeautifulSoup(page.content, 'html.parser')
 
-f = open("indeedSearch.txt", "x")
-f.write(page.text)
-f.close()
+#each posting has a job card and these cards have the jobsearch-serpjobcard class
+
+job_cards = soup.findAll("div", {"class":"jobsearch-SerpJobCard"})
+
+#for location, it can exist as both a span or a div for whatever reason
+
+for job_elem in job_cards:
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('span', class_='company')
+    location_elem = job_elem.find(['div','span'], class_='location accessible-contrast-color-location')
+    
+    print(title_elem.text)
+    print(company_elem.text)
+    print(location_elem.text)
+
+ 
